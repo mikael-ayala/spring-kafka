2d3af1688ec4dccf63eb073ccf802a46b60351be
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-08-16 14:54:03 -0300

    initial commit

---
+++ src/main/java/dio/expertostech/rest_kafka/RestKafkaApplication.java
@@ -0,0 +1,15 @@
+package dio.expertostech.rest_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@SpringBootApplication
+public class RestKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/expertostech/rest_kafka/RestKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.expertostech.rest_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
